5d8a383bf16ef21f1d7736e93545a9ba294e9968
shelton <shltnsimbi@gmail.com>
2023-05-11 12:15:34 +0200

    Initial Commit

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <header class="text-center text-white bg-blue-800 p-4 mb-10">
+            <div class="text-3xl md:text-5xl font-bold mb-3">
+                <i class="fa fa-viruses"></i>
+                Covid 19 Tracker</div>            
+            <p>
+                API by 
+                <a href="https://covid19api.com" class="text-blue-300" target="_blank">covid19.com</a>
+            </p>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"HeaderComponent"
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate"/>
+  </main>
+
+  <main v-else class="flex flex-col align-center text-center justify-center">
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data
+    </div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="">
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DataTitle from '@/components/DataTitle.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name:'HomeView',
+  components:{
+    DataTitle
+  },
+  data(){
+    return {
+      loading:true,
+      title:"Global",
+      dataDate:'',
+      stats:{},
+      countries:[],
+      //loadingImage:require('')
+    }
+  },
+  methods:{
+    async fetchCovidData(){
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    }
+  },
+  async created(){
+    const data = await this.fetchCovidData()
+    
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.countries
+    this.loading = false
   }
 }
 </script>
